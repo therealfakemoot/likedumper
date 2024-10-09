7fed97ec2e1b5e314bbc720f9cf9e28e59c6d186
Nick Dumas <nobodys@business.com>
2024-10-09 16:37:32 -0400

    Let's not hand-roll oauth2

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/therealfakemoot/likedumper
 
 go 1.21.0
 
-require github.com/pelletier/go-toml/v2 v2.2.3
+require (
+	github.com/pelletier/go-toml/v2 v2.2.3
+	golang.org/x/oauth2 v0.23.0
+)
--- main.go
+++ main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"context"
 	"fmt"
-	// "net/http"
 	"log/slog"
 	"os"
 
 	"github.com/pelletier/go-toml/v2"
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/spotify"
 )
 
 type Config struct {
@@ -29,6 +31,16 @@ func LoadConfigFile(fn string) (Config, error) {
 	return c, nil
 }
 
+type SpotifyTracksResponse struct {
+	Href     string
+	Next     string
+	Previous string
+	Limit    int
+	Offset   int
+	Total    int
+	Items    []SpotifyTrack
+}
+
 func main() {
 	logger := slog.New(slog.NewTextHandler(os.Stderr, nil))
 	conf, err := LoadConfigFile("config.toml")
@@ -37,5 +49,38 @@ func main() {
 	}
 
 	fmt.Printf("%#+v\n", conf)
+
+	ctx := context.Background()
+	oauthConf := &oauth2.Config{
+		ClientID:     conf.ID,
+		ClientSecret: conf.Secret,
+		Scopes:       []string{"user-library-read"},
+		Endpoint:     spotify.Endpoint,
+	}
+
+	verifier := oauth2.GenerateVerifier()
+
+	url := oauthConf.AuthCodeURL("state", oauth2.AccessTypeOffline, oauth2.S256ChallengeOption(verifier))
+	fmt.Printf("Visit the URL for the auth dialog: %q\n", url)
+
+	var code string
+	if _, err := fmt.Scan(&code); err != nil {
+		logger.Error("did not receive valid auth code")
+		return
+	}
+
+	tok, err := oauthConf.Exchange(ctx, code, oauth2.VerifierOption(verifier))
+	if err != nil {
+		logger.Error("unable to verify PKCE")
+		return
+	}
+	client := oauthConf.Client(ctx, tok)
+	resp, err := client.Get("https://api.spotify.com/v1/me/tracks")
+	if err != nil {
+		logger.Error("unable to fetch Tracks")
+		return
+	}
+
+	dec := json.NewDecoder(resp.Body)
 
 }
