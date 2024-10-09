a49879a18548f7786f86a8973ce16a1242f5af83
Nick Dumas <nobodys@business.com>
2024-10-09 15:52:29 -0400

    First draft

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/therealfakemoot/likedumper
+
+go 1.21.0
+
+require github.com/pelletier/go-toml/v2 v2.2.3
---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	// "net/http"
+	"log/slog"
+	"os"
+
+	"github.com/pelletier/go-toml/v2"
+)
+
+type Config struct {
+	ID     string `toml:"SPOTIFY_APP_ID"`
+	Secret string `toml:"SPOTIFY_APP_TOKEN"`
+}
+
+func LoadConfigFile(fn string) (Config, error) {
+	var c Config
+	f, err := os.Open(fn)
+	if err != nil {
+		return c, fmt.Errorf("couldn't open config file: %w", err)
+	}
+	dec := toml.NewDecoder(f)
+	err = dec.Decode(&c)
+	if err != nil {
+		return c, fmt.Errorf("error parsing config file: %w", err)
+	}
+
+	return c, nil
+}
+
+func main() {
+	logger := slog.New(slog.NewTextHandler(os.Stderr, nil))
+	conf, err := LoadConfigFile("config.toml")
+	if err != nil {
+		logger.With("error", err).Error("error loading config file")
+	}
+
+	fmt.Printf("%#+v\n", conf)
+
+}
